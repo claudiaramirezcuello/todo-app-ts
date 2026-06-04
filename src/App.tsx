e24cbb429db36d81ac87d0b74c29436605fa4302
clàudia <claudiaramirezcuello@gmail.com>
2026-06-04 12:00:47 +0200

    añadido estado

--- src/App.tsx
+++ src/App.tsx
@@ -26,6 +26,7 @@ const mockTodos = [
 const App = (): JSX.Element => {
   const [todos, setTodos] = useState(mockTodos) // inicializar la lista de todos con mockTodos, setTodos actualiza la lista de todos
   const [filterSelected, setFilterSelected] = useState<FilterValue>(TODO_FILTERS.ALL) // el filtro por defecto es 'all', setFilterSelected actualiza el filtro seleccionado
+  const [userName, setUserName] = useState('Clàudia')
 
   const activeCount = todos.filter(todo => !todo.completed).length
   const completedCount = todos.length - activeCount
