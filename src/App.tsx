2068c09d0c2dcd38ddb2c085b4fdd4f14cfeee88
clàudia <claudiaramirezcuello@gmail.com>
2026-06-08 10:49:15 +0200

    definifo rutas

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React, { JSX } from 'react';
 import Home from './pages/Home';
+import { Routes, Route } from 'react-router-dom';
+import About from './pages/About';
 
 const App = (): JSX.Element => {
-  return <Home />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  );
 };
 
 export default App;
