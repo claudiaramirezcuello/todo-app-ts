5e25e92733d78c896e6e32d5c68f55241cf8b7e4
clàudia <claudiaramirezcuello@gmail.com>
2026-06-08 10:47:10 +0200

    creado página About

---
+++ src/pages/About.tsx
@@ -0,0 +1,12 @@
+import React, { JSX } from 'react';
+
+const About = (): JSX.Element => {
+  return (
+    <div>
+        <h1>Acerca de esta app</h1>
+        <p>Esta aplicación de tareas fue creada por Clàudia para aprender React y TypeScript.</p>
+    </div>
+  );
+};
+
+export default About;
