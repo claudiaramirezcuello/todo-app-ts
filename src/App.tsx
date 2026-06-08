06852b7185ef0cc50fdce775937f226842bac399
clàudia <claudiaramirezcuello@gmail.com>
2026-06-08 10:53:45 +0200

    creado y añadido navegación

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import React, { JSX } from 'react';
 import Home from './pages/Home';
 import { Routes, Route } from 'react-router-dom';
 import About from './pages/About';
+import Nav from './components/Nav';
 
 const App = (): JSX.Element => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-    </Routes>
+    <>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
   );
 };
 
---
+++ src/components/Nav.tsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom';
+import React, { JSX } from 'react';
+
+const Nav = (): JSX.Element => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
